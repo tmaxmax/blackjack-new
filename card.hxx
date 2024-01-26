03cf62995bb1618fa55923cbdd839c3c90e0a016
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2024-01-26 18:51:51 +0200

    Add documentation for Card::empty

--- card.hxx
+++ card.hxx
@@ -21,6 +21,11 @@ public:
     // operator<< writes a string representation of the card to the stream.
     friend auto operator<<(std::ostream&, Card) -> std::ostream&;
 
+    // The empty card is a numeric Clubs card with value 0.
+    // None of its properties carry any special value – it can be used
+    // as a sentinel value. In the places where the empty card is used
+    // std::optional<Card> would conceptually be a better fit, but
+    // ergonomically it would sucks.
     static const Card empty;
 private:
     int value_;
